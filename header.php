ef4c899ddaa85000836d0fe26a7951728b29c7de
AlexGuillemine <alex.guillemine@gmail.com>
2015-04-14 13:49:12 +0200

    version définitive avant intégration bbd

---
+++ header.php
@@ -0,0 +1,9 @@
+<?php
+		header ('Content-type:text/html; charset=utf-8');
+?>
+<header class="row">	
+		<img src="images/logo_beerecycle.jpg" class="col-lg-2" alt="logo_beerecycle"/>
+    <h1 class="titre_banniere col-lg-offset-1 col-lg-offset-8">Management interface of the Beerecycle activity</h1>
+	</header>
+
+
